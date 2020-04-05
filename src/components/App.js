94e6a8406ab331253a0012dc047bc2aeb9426374
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-05 18:00:46 +0100

    Add font awesome to project and nav bar to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './navigation/navbar/NavBar.component';
 import Hero from './hero/Hero.component';
 
 
 const App = () => {
   return (
-    <Hero />
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 };
 
