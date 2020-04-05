16841c1db0834d43918d4d53f8439996d77b764b
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-05 18:01:07 +0100

    Structure mobile navbar

--- src/components/navigation/navbar/NavBar.component.jsx
+++ src/components/navigation/navbar/NavBar.component.jsx
@@ -4,9 +4,11 @@ import './navbar.styles.scss';
 const Navbar = () => {
   return (
     <div className="nav-bar row">
-      <div className="nav-left"></div>
-      <div className="nav-center"></div>
-      <div className="nav-right"></div>
+      <div className="nav-left"><i className="menu-icon"></i></div>
+      <div className="nav-center row"><i className="home-icon"></i><p className="nav-text">schoolclosures.org</p></div>
+      <div className="nav-right"><i className="search-icon"></i></div>
     </div>
   )
 };
+
+export default Navbar;
