b9203dcd76060395fec0d6fc33715cc4f2d3fdc2
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-05 17:41:05 +0100

    Add navbar component

---
+++ src/components/navigation/navbar/NavBar.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.styles.scss';
+
+const Navbar = () => {
+  return (
+    <div className="nav-bar row">
+      <div className="nav-left"></div>
+      <div className="nav-center"></div>
+      <div className="nav-right"></div>
+    </div>
+  )
+};
