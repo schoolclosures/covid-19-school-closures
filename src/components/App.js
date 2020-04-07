61c6f1d2e02da7180c808ad169d60c64132e58a3
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-07 18:36:49 +0100

    Add components to app

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import Hero from './hero/Hero.component';
 import Footer from './footer/Footer.component';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MenuModal from '../containers/menu-modal/MenuModal.container';
+import Contact from './contact/Contact.component';
+import About from './about/About.component';
+import ForParents from './hero-pages/for-parents/ForParents.component';
+import ForTeachers from './hero-pages/for-teachers/ForTeachers.component';
 
 
 const App = () => {
@@ -12,7 +16,11 @@ const App = () => {
     <Router>
       <Navbar />
       <MenuModal />
-      <Route path="/" component={Hero}/>
+      <Route exact path="/" component={Hero} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/for_teachers" component={ ForTeachers }/>
+      <Route exact path="/for-parents" component={ ForParents }/>
       <Footer />
     </Router>
     </>
