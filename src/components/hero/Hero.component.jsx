21190ce8b2f3856217562490e152679f271a86d5
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-07 18:37:38 +0100

    Write button link component

--- src/components/hero/Hero.component.jsx
+++ src/components/hero/Hero.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Button from '../button/Button.component';
+import ButtonLink from '../button-link/ButtonLink.component';
 import './hero.styles.scss';
 
 const Hero = () => {
   return (
-    <div className="hero-section column">
+    <div className="hero-section column section">
       <p className="hero-text">Information and free tutoring for families and teachers during COVID-19 school closures.</p>
       <div className="hero-cta-holder row">
-        <Button buttonAction={"For Parents"} outlineColor={"white"}/>
-        <Button buttonAction={"For Teachers"} outlineColor={"white"}/>
+        <ButtonLink buttonAction={"For Parents"} outlineColor={"white"} path="/for-parents" />
+        <ButtonLink buttonAction={"For Teachers"} outlineColor={"white"} path="/for-teachers" />
       </div>
     </div>
   )
