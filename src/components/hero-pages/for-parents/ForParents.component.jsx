513d380fbd7835d654d7f31ca7322b3f72059fdb
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-08 18:08:23 +0100

    Add article list and prop type checking to for parents and for teachers

--- src/components/hero-pages/for-parents/ForParents.component.jsx
+++ src/components/hero-pages/for-parents/ForParents.component.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import HeroHeader from '../hero-header/HeroHeader.component';
+import ArticleList from '../articles/article-list/ArticleList.component';
 
-const ForParents = () => {
+const ForParents = ( { articles } ) => {
   return (
     <div className="hero-page-section">
       <HeroHeader heroType={ "Parents" }/>
+      <ArticleList articles={articles}/>
     </div>
   )
 };
+
+ForParents.defaultProps = {
+  articles: [{ "": "" }]
+};
+
+ForParents.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.objectOf(String))
+}
 
 export default ForParents;
