ad1de1c5ddab6de14a64cdef2233f27fab89b1a4
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-16 15:29:22 +0100

    Set base url for axios and write receive team members type

--- src/actions/components.actions.js
+++ src/actions/components.actions.js
@@ -1,4 +1,11 @@
+import axios from 'axios';
+
 export const types = {
   OPEN_MODAL: "OPEN_MODAL",
   CLOSE_MODAL: "CLOSE_MODAL",
+  RECEIVE_TEAM_MEMBERS: "RECEIVE_TEAM_MEMBERS",
 };
+
+export const API = axios.create({
+  baseURL: `https://schoolclosures-api.herokuapp.com`,
+}); 
