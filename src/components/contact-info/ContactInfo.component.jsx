199da00b49d9376fe9e2e4cf1361f730695fd122
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-17 18:24:56 +0100

    Write contact info component

--- src/components/contact-info/ContactInfo.component.jsx
+++ src/components/contact-info/ContactInfo.component.jsx
@@ -5,11 +5,13 @@ const ContactInfo = () => {
   return (
     <div className="contact-info">
       <p className="heading">Seeking support from a fellow parent?</p>
-      <div className="row contact-detail">
-        <i className="phone-icon"></i><p className="sub-text">(145) - 243 - 7809</p>
+      <div className="column contact-detail">
+        <i className="phone-icon"></i>
+        <p className="sub-text">(145) - 243 - 7809</p>
       </div>
-      <div className="row contact-detail">
-        <i className="email-icon"></i><p className="sub-text">hello@schoolclosures.org</p>
+      <div className="column contact-detail">
+        <i className="email-icon"></i>
+        <p className="sub-text">hello@schoolclosures.org</p>
       </div>
     </div>
   )
