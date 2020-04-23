c1f6229b6b40441e0c036c4a36030ba12ecd2c8f
chanwkkk <chanwk18@hotmail.com>
2020-04-23 14:41:29 -0600

    fix the mixup links of social network accounts

--- src/components/social-icons/SocialIcons.component.jsx
+++ src/components/social-icons/SocialIcons.component.jsx
@@ -6,10 +6,10 @@ const SocialIcons = () => {
       <div className="footer-social-icons">
       <p className="heading">Find us on social media</p>
       <div className="social-icons row">
-        <a href="https://www.facebook.com/schoolclosures" target="_blank" rel="noreferrer noopener">
+        <a href="https://twitter.com/covid19kids" target="_blank" rel="noreferrer noopener">
           <i className="footer-social-icon twitter-icon"></i>
         </a>
-        <a href="https://twitter.com/covid19kids" target="_blank" rel="noreferrer noopener">
+        <a href="https://www.facebook.com/schoolclosures" target="_blank" rel="noreferrer noopener">
           <i className="footer-social-icon facebook-icon"></i>
         </a>
         <a href="mailto:manisha@modulo.app">
