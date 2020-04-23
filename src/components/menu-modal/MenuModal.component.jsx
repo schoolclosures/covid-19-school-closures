a0de959df1a25adeee7af0308de215086481f098
Samantha Banchik <sam11b@berkeley.edu>
2020-04-22 17:24:53 -0700

    finished using local state

--- src/components/menu-modal/MenuModal.component.jsx
+++ src/components/menu-modal/MenuModal.component.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './menu-modal.styles.scss';
 
 const MenuModal = ({ modalIsOpen, closeModal }) => {
+  const [open, setOpenBool] = useState(true);
+  useEffect(()=> {
+    if (!open) {
+      closeModal();
+      setOpenBool(true);
+    }
+  });
   return (
     <div className={`menu-modal column ${modalIsOpen ? "open" : ""}`}>
       <button onClick={() => closeModal()} className="no-bg-btn">
         <i className="close-icon"></i>
       </button>
-      <Link className="menu-modal-link" to="/">Home</Link>
-      <Link className="menu-modal-link" to="/for-parents">For Parents</Link>
-      <Link className="menu-modal-link" to="/for-teachers">For Teachers</Link>
-      <Link className="menu-modal-link" to="/about">About Us</Link>
-      <Link className="menu-modal-link" to="/contact">Contact Us</Link>
+      <Link onClick={() => setOpenBool(false)} className="menu-modal-link" to="/">Home</Link>
+      <Link onClick={() => setOpenBool(false)} className="menu-modal-link" to="/for-parents">For Parents</Link>
+      <Link onClick={() => setOpenBool(false)} className="menu-modal-link" to="/for-teachers">For Teachers</Link>
+      <Link onClick={() => setOpenBool(false)} className="menu-modal-link" to="/about">About Us</Link>
+      <Link onClick={() => setOpenBool(false)} className="menu-modal-link" to="/contact">Contact Us</Link>
     </div>
   )
 };
