850140f37b60301a054757b712159efb8943694f
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-05-04 15:29:44 +0100

    Refactor paginations into a component and apply on team members list

--- src/components/team-members-list/TeamMembersList.component.jsx
+++ src/components/team-members-list/TeamMembersList.component.jsx
@@ -1,104 +1,62 @@
-import React, { useState, useEffect }                                          from 'react';
+import React, { useState, useEffect } from 'react';
 import TeamMember from '../team-member/TeamMember.component';
 import PropTypes from 'prop-types';
 import './team_member_list.styles.scss';
+import PaginationControl from './pagination/pagination-controls/PaginationControl.component';
 
 
-const TeamMembersList = ( { teamMembers} ) => { 
-  // 8 images per page 
-  const numPages = Math.ceil(teamMembers.length/8);
-  const [currPage, changePage] = useState(1);
-  const [currentPageMembers, changeMembers] = useState(teamMembers.slice(0, 8));
+const TeamMembersList = ( { teamMembers } ) => { 
+  // 8 items per page 
+  const numPerPage = 8;
+  const numOfPages = Math.ceil(teamMembers.length/numPerPage);
+  const [currentPage, changePage] = useState(1);
+  const [lowerBound, setLowerBound] = useState(0);
+  const [currentPageMembers, changeMembers] = useState(teamMembers.slice(0, numPerPage));
+  const [upperBound, setUpperBound] = useState(numPerPage);
+
+  function handlePageChange(value) {
+    changePage(value)
+    setLowerBound(numPerPage * (value - 1));
+    setUpperBound(numPerPage * value);
+  }
   
   useEffect(()=> {
-    if (currPage == 1) {
-      changeMembers(teamMembers.slice(0, 8));
-    }
-    else if (currPage == 2) {
-      changeMembers(teamMembers.slice(8, 16));
-    }
-    else if (currPage == 3) {
-      changeMembers(teamMembers.slice(16, 24));
-    }
-    else if (currPage == 4) {
-      changeMembers(teamMembers.slice(24, 32));
-    }
-    else if (currPage == 5) {
-      changeMembers(teamMembers.slice(32, 40));
-    }
-    else if (currPage == 6) {
-      changeMembers(teamMembers.slice(40, 48));
-    }
-    else if (currPage == 7) {
-      changeMembers(teamMembers.slice(48, 56));
-    }
-    else if (currPage == 8) {
-      changeMembers(teamMembers.slice(56, 64));
-    }
-    else if (currPage == 9) {
-      changeMembers(teamMembers.slice(64));
-    }
-  }, [currPage]);
+    changeMembers(teamMembers.slice(lowerBound, upperBound));
+  }, [teamMembers, upperBound, lowerBound]);
 
   function prevPage() {
-    if (currPage > 1) {
-      changePage(currPage-1);
+    if (currentPage > 1) {
+      changePage(currentPage-1);
+      setLowerBound(lowerBound - numPerPage);
+      setUpperBound(upperBound - numPerPage);
     }
   }
 
   function nextPage() {
-    if (currPage < numPages) {
-      changePage(currPage+1);
-      console.log(currPage);
+    if (currentPage < numOfPages) {
+      changePage(currentPage+1);
+      setLowerBound(lowerBound + numPerPage);
+      setUpperBound(upperBound + numPerPage);
     }
   }
   
   return (
       <>
         <h2 className="sub-heading pink-underline">Who we are</h2>
-         <div className="row-with-wrap team-members-list"> 
-            { currentPageMembers && currentPageMembers.map( teamMember => <TeamMember key={teamMember.attrs.id} teamMember={teamMember} /> ) }
-          </div>
-        <div className="page-navbar-team-members">
-         <div className="scroll-left arrow-right-icon" style={{transform: 'rotate(180deg)'}} onClick={() => prevPage()}></div> 
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 1) ? '#6FA8FF': null, 
-                  color: (currPage == 1) ? '#080808': null}}
-          onClick={() => changePage(1)}> 1 </div>
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 2) ? '#6FA8FF': null, 
-                  color: (currPage == 2) ? '#080808': null}}
-          onClick={() => changePage(2)}> 2 </div>
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 3) ? '#6FA8FF': null, 
-                  color: (currPage == 3) ? '#080808': null}}
-          onClick={() => changePage(3)}> 3 </div>
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 4) ? '#6FA8FF': null, 
-                  color: (currPage == 4) ? '#080808': null}}
-          onClick={() => changePage(4)}> 4 </div>
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 5) ? '#6FA8FF': null, 
-                  color: (currPage == 5) ? '#080808': null}}
-          onClick={() => changePage(5)}> 5 </div>
-         <div className="page-number" 
-         style={{backgroundColor: (currPage == 6) ? '#6FA8FF': null, 
-                color: (currPage == 6) ? '#080808': null}}
-          onClick={() => changePage(6)}> 6 </div>
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 7) ? '#6FA8FF': null, 
-                color: (currPage == 7) ? '#080808': null}}
-          onClick={() => changePage(7)}> 7 </div>
-         <div className="page-number" 
-          style={{backgroundColor: (currPage == 8) ? '#6FA8FF': null, 
-                color: (currPage == 8) ? '#080808': null}}
-          onClick={() => changePage(8)}> 8 </div>
-          <div className="page-number" 
-          style={{backgroundColor: (currPage == 9) ? '#6FA8FF': null, 
-                color: (currPage == 9) ? '#080808': null}}
-          onClick={() => changePage(9)}> 9 </div>
-         <div className="scroll-right arrow-right-icon" onClick={() => nextPage()}></div>
+        <div className="row-with-wrap team-members-list"> 
+          { currentPageMembers && currentPageMembers.map( teamMember => <TeamMember key={teamMember.attrs.id} teamMember={teamMember} /> ) }
         </div>
+        <PaginationControl 
+          items={teamMembers}
+          numPerPage={8}
+          handlePageChange={handlePageChange}
+          upperBound={upperBound}
+          lowerBound={lowerBound}
+          handleNext={nextPage}
+          handlePrev={prevPage}
+          numOfPages={numOfPages}
+          currentPage={currentPage}
+        />
       </>
     )
 };
---
+++ src/components/team-members-list/pagination/pagination-controls/PaginationControl.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PageNumber from '../page-number/PageNumber.component';
+
+const PaginationControl = ( { currentPage, handlePageChange, handlePrev, handleNext, numOfPages} ) => {
+
+  const pages = [];
+
+  for(let i = 1; i <= numOfPages; i++) {
+    pages.push(i)
+  };
+
+  return (
+    <div className="page-navbar-team-members row">
+      <button className="scroll-left no-bg-btn" onClick={() => handlePrev()}><i className="arrow-left-icon"></i></button> 
+      { 
+      pages && pages.map(page => <PageNumber value={page} currentPage={currentPage} handleClick={handlePageChange}/>)
+      }
+      <button className="scroll-right no-bg-btn" onClick={() => handleNext()}><i className="arrow-right-icon"></i></button>
+    </div>
+  )
+};
+
+export default PaginationControl;
