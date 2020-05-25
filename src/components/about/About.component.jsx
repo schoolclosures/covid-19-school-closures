36d028e987d7e781208cb15e6667bfaae9355849
Kevin Kennis <kevinvkennis@gmail.com>
2020-05-24 21:20:58 -0700

    add partners

--- src/components/about/About.component.jsx
+++ src/components/about/About.component.jsx
@@ -18,13 +18,11 @@ class About extends Component {
           <h2 className="main-heading heading">Education doesn’t stop when schools close.</h2>
           <h2 className="sub-heading heading pink-underline">About Us</h2>
           <p className="about-summary">
-            Schoolclosures.org was crafted by a group of 
-            volunteers made of teachers, students, full-time 
-            workers, and many others. By the likes of people 
-            coming together through difficult times, we’ve 
-            created a resource to ensure that even though we 
-            may not be physically present with each other, 
-            we can still be connected to one another.
+            On March 9th, we realized millions of families were facing the prospect of having to teach their kids from home for the first time. As parents and educators, we wanted to find a way to help families through this difficult transition and beyond. We saw a lot of efforts getting started, but no unified response to the crisis. We knew a centralized place for information and support was critical.
+ 
+            We reached out to our friends with experience in education, food services, financial security and emotional support services to coordinate efforts to help families. Overnight, we launched a free helpline staffed by volunteers with expertise in remote learning. Parents and teachers across the country started calling in and asking for help. In our first week, we were able to provide a Chromebook to a family in Florida, groceries to a mom with a new baby in California and a volunteer tutor to a family with special needs children. 
+ 
+            Since then, our coalition has grown to over 200 volunteers and 150 organizations including the Crisis Text Line, Khan Academy and Revolution Foods. Through our hotline and the information on our website, our goal is to make sure every family and teacher knows there is a place to get help and emotional support. We are committed to every person that reaches out to us - holding their hand until they find a solution to whatever problem they are facing, whether it is customizing their learning plan, finding a laptop or a healthy meal.
           </p>
         </div>
   
--- src/components/partners/partners-list/PartnersList.component.jsx
+++ src/components/partners/partners-list/PartnersList.component.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Partner from '../partner/Partner.component';
+import PartnerLogos from '../../../assets/images/partner-logos.png';
 import './partners_list.styles.scss';
 
 const PartnersList = ( { partners } ) => {
   return (
     <>
       <div className="partners-list row-with-wrap">
-        <Partner name={"Khan Academy"}/>
-        <Partner name={"Dexter"}/>
-        <Partner name={"Prenda"}/>
-        <Partner name={"Twillo"}/>
-        <Partner name={"ClassDojo"}/>
-        <Partner name={"Code"}/>
-        <Partner name={"Crisis Text Line"}/>
-        <Partner name={"Learn Capital"}/>
-        <Partner name={"LTSE"}/>
-        <Partner name={"Modulo"}/>
-        <Partner name={"Okta"}/>
-        <Partner name={"Outschool"}/>
+        <img src={PartnerLogos} />
       </div>
     </>
   )
